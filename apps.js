02cb816bae6895a8280be1a905a4712182587b39
boburbekburkhonov <zafaroviccc@gmail.com>
2023-05-13 11:52:09 +0500

    feat: 1232

---
+++ apps.js
@@ -0,0 +1,25 @@
+const userName = prompt("Ismingiz", "Joe");
+const userSurname = prompt("Familyangiz", "Cole");
+const userAge = prompt("Yoshingiz", "2000");
+const userSport = prompt("Yaxshi Ko'rgan Sportingiz", "futbol");
+const userJob = prompt("Kasbingiz", "Developer");
+const userColor = prompt("Yaxshi Ko'rgan Rangiz", "Qora");
+const userSubject = prompt("Yaxshi Ko'rgan Faniz", "Math");
+
+console.log("Ismingiz: " + userName + "\n" + "Familyangiz: " + userSurname + "\n" + "Yoshingiz: " + (2020 - userAge) + "\n" + "Sportingiz: " + userSport + "\n" + "Kasbingiz: " + userJob + "\n" + "Rangiz: " + userColor + "\n" + "Faningiz: " + userSubject);
+
+const country = "Uzbekistan";
+const contient = "Uzbek";
+const population = "35 mln";
+
+console.log("Country: " + country + "\n" + "Contient: " + contient + "\n" + "Population: " + population);
+
+const isIsland = "Islandiya";
+let language;
+language = "uzbek"
+// console.log(language);
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ js/apps.js
@@ -0,0 +1,95 @@
+// TASK-1
+
+// const userName = prompt("Ismingizni kiriting")
+// const moneyType = prompt("Mablag'ingiz $ yoki sum da mi");
+// const userMoney = prompt(`${userName} Jami mablag'ingizni kiriting`) *1;
+
+// let pilotDollar= 500;
+//     hotelDollar = 250;
+//     museumDollar = 124;
+//     totalDollar= pilotDollar + hotelDollar + museumDollar;
+
+// let pilot = 500 * 10896.61;
+//     hotel = 500 * 250;
+//     museum = 120 * 12247.79;
+
+//     total = pilot + hotel + museum;
+
+// if (moneyType == "$") {
+//   console.log(`Mablag'ingiz dollarda: ${userMoney}$,`);
+
+//   if (userMoney >= totalDollar) {
+//     console.log(`Oq yo'l ${userName}!`);
+//   } else {
+//     console.log(`${userName} ozgina sabr qilish kerak bo’lar ekan!`);
+//   }
+// }
+
+// if (moneyType == "sum") {
+//   console.log(`Mablag'ingiz sumda: ${userMoney} ming.sum,`);
+
+//   if (userMoney >= total) {
+//     console.log(`Oq yo'l ${userName}!`);
+//   } else {
+//     console.log(`${userName} ozgina sabr qilish kerak bo’lar ekan!`);
+//   }
+// }
+
+
+// TASK-2
+
+// const markWeight = prompt("Mark Og'irligingizni kiriting") *1;
+// const markHeight = prompt("Mark Bo'yingizni kiriting") *1;
+// const markBmi = markWeight / (markHeight * markHeight);
+
+// const johnWeight = prompt("John Og'irligingizni kiriting") *1;
+// const johnHeight = prompt("John Bo'yingizni kiriting") *1;
+// const johnBmi = johnWeight / (johnHeight * johnHeight);
+
+// const markCalculation = markBmi - johnBmi;
+// const johnCalculation = johnBmi - markBmi;
+
+// console.log(`Markniki:
+// Og'irligi: ${markWeight}kg,
+// Bo'yi: ${markHeight}sm,
+// BMI: ${markBmi}`);
+
+// console.log(`Johnniki:
+// Og'irligi: ${johnWeight}kg,
+// Bo'yi: ${johnHeight}sm,
+// BMI: ${johnBmi}`);
+
+// if (markBmi > johnBmi) {
+//   alert("Markning BMI katta ekan")
+// } else {
+//   alert("Johnning BMI katta ekan")
+// }
+
+// TASK-3
+
+// const markWeight = prompt("Mark Og'irligingizni kiriting") *1;
+// const markHeight = prompt("Mark Bo'yingizni kiriting") *1;
+// const markBmi = markWeight / (markHeight * markHeight);
+
+// const johnWeight = prompt("John Og'irligingizni kiriting") *1;
+// const johnHeight = prompt("John Bo'yingizni kiriting") *1;
+// const johnBmi = johnWeight / (johnHeight * johnHeight);
+
+// const markCalculation = markBmi - johnBmi;
+// const johnCalculation = johnBmi - markBmi;
+
+// console.log(`Markniki:
+// Og'irligi: ${markWeight}kg,
+// Bo'yi: ${markHeight}sm,
+// BMI: ${markBmi}`);
+
+// console.log(`Johnniki:
+// Og'irligi: ${johnWeight}kg,
+// Bo'yi: ${johnHeight}sm,
+// BMI: ${johnBmi}`);
+
+// if (markBmi > johnBmi) {
+//   console.log(`Markning BMI ${markBmi}, Johnnikidan ${markCalculation}ga yuqori.`);
+// } else {
+//   console.log(`Johnning BMI ${johnBmi}, Marknikidan ${johnCalculation}ga yuqori.`);
+// }
